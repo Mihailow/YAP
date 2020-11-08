c96e063c93eba4c7fac2e2b8edcee75e50983fca
Мишка <bramnikmisha21@gmail.com>
2020-11-08 22:36:30 +0300

    .

---
+++ pr4/p4/LookupCollection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace LookupCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            list["Estados Unidos"] = "United States of America";
+            list["Canada"] = "Canada";
+            list["Espana"] = "Spain";
+            Console.WriteLine(list["espana"]);
+            Console.WriteLine(list["CANADA"]);
+            Console.Read();
+
+        }
+    }
+}
