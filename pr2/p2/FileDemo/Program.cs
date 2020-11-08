908787ea33893952c609a7fc2b74dd35f4057801
Мишка <bramnikmisha21@gmail.com>
2020-11-08 22:15:40 +0300

    .

---
+++ pr2/p2/FileDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText(@"d:\newfile.txt"); 
+            writer.WriteLine("This is my new file");
+            writer.WriteLine("Do you like its format?"); 
+            writer.Close();
+
+            StreamReader reader = File.OpenText(@"d:\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+
+        }
+    }
+}
